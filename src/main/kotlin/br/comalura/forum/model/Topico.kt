3fad8d9bb2dd83ecf7903a7a9c9daa589555b704
othiagomanhaes <othiagomanhaes@gmail.com>
2023-11-01 13:36:14 -0300

    criação do docker-compose, modificação na query especial.

--- src/main/kotlin/br/comalura/forum/model/Topico.kt
+++ src/main/kotlin/br/comalura/forum/model/Topico.kt
@@ -4,7 +4,7 @@ import jakarta.persistence.*
 import java.time.LocalDateTime
 
 @Entity
-class Topico (
+data class Topico (
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     var titulo: String,
